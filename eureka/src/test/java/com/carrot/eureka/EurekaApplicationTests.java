706bee9cf59bed35990978df0199901d54338351
zzoo0123 <0123suh@gmail.com>
2024-10-23 13:19:41 +0900

    chore: init

---
+++ eureka/src/test/java/com/carrot/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrot.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
